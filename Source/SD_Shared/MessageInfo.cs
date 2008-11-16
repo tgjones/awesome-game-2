36a06b8da5aa08a761cb2cdd4cb4e180e4bd3e70
Daniel Foster <daniel@34sp.com>
2008-11-16 13:12:08 +0000

    MessageInfo.cs

---
+++ Source/SD_Shared/MessageInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SD.Shared
+{
+    public class MessageInfo
+    {
+        public int Id { get; set; }
+        public int ToPlayerId { get; set; }
+        public int FromPlayerId { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public PlayerInfo FromPlayer { get; set; }
+        public PlayerInfo ToPlayer { get; set; }
+
+        public MessageInfo() { }
+
+        public MessageInfo(int id, int toPlayerId, int fromPlayerId, string subject, string body)
+            : this()
+        {
+            Id = id;
+            ToPlayerId = toPlayerId;
+            FromPlayerId = fromPlayerId;
+            Subject = subject;
+            Body = body;
+        }
+
+        public MessageInfo(int id, int toPlayerId, int fromPlayerId, string subject, string body, PlayerInfo fromPlayer, PlayerInfo toPlayer)
+            : this(id, toPlayerId, fromPlayerId, subject, body)
+        {
+            FromPlayer = fromPlayer;
+            ToPlayer = ToPlayer;
+        }
+    }
+}
