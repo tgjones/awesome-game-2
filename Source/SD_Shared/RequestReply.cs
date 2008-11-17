71e48728bd3a212521d3f381ff1b3f7d341e1876
Barney Hawes <barney@barneyhawes.com>
2008-11-17 18:57:18 +0000

    Added file I forgot to add (and it was such a small one...).

---
+++ Source/SD_Shared/RequestReply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SD.Shared
+{
+    public class RequestReply
+    {
+        public bool Success;
+        public string ErrorMessage;
+    }
+}
